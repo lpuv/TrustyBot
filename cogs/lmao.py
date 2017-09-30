3066d32cfadcc534accc9e505369e8b8bdc50534
TrustyJAID <TrustyJAID@gmail.com>
2017-09-29 22:01:45 -0600

    updated emoji posting to save location

--- cogs/lmao.py
+++ cogs/lmao.py
@@ -6,21 +6,18 @@ class LMAO():
 
     async def on_message(self, message):
         channel = message.channel
-        if message.server.id != "344325990849314816":
+        if message.server.id not in ["344325990849314816", "321105104931389440"]:
             return
         if message.author.bot:
             return
         new_msg = ""
-        emojis = []
-        for emoji in message.server.emojis:
-            if emoji.id in message.content:
-                emojis.append(":" + emoji.name + ":" + emoji.id)
-                new_msg += "<:" + emoji.name + ":" + emoji.id + "> "
-        if new_msg != "":
-            new_msg = await self.bot.send_message(channel, new_msg)
-            for emoji in emojis:
-                await self.bot.add_reaction(new_msg, emoji)
-                await self.bot.add_reaction(message, emoji)
-
+        emojis = lambda: [x.rpartition(">")[0].partition("<")[2] for x in message.content.split(' ') if x.startswith('<:') and x.endswith('>')]
+        if emojis() == []:
+            return
+        new_msg = "".join("<" + emoji + ">" for emoji in emojis())
+        new_msg = await self.bot.send_message(channel, new_msg)
+        for emoji in emojis():
+            await self.bot.add_reaction(new_msg, emoji)
+            await self.bot.add_reaction(message, emoji)
 def setup(bot):
     bot.add_cog(LMAO(bot))
