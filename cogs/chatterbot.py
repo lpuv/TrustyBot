6ff0f3afdea0c950d4acfbd816af13ba94a1570c
TrustyJAID <TrustyJAID@gmail.com>
2017-10-03 11:51:37 -0600

    Added ability for bot to learn from all messages it sees on discord

--- cogs/chatterbot.py
+++ cogs/chatterbot.py
@@ -24,7 +24,7 @@ class Chatterbot():
                                           atabase="data/chatterbot/db")
         self.chatbot.set_trainer(ListTrainer)
 
-    @commands.command(no_pm=True, invoke_without_command=True, pass_context=True)
+    @commands.group(no_pm=True, invoke_without_command=True, pass_context=True)
     async def chatterbot(self, ctx, *, message):
         """Talk with cleverbot"""
         author = ctx.message.author
@@ -32,15 +32,58 @@ class Chatterbot():
         response = self.chatbot.get_response(message)
         await self.bot.send_message(channel, response)
 
+    @chatterbot.command(pass_context=True)
+    @checks.mod_or_permissions(manage_channels=True)
+    async def toggle(self, ctx):
+        """Toggles reply on mention"""
+        server = ctx.message.server
+        if server.id not in self.settings:
+            self.settings[server.id] = {"TOGGLE" : True, "channel":""}
+        self.settings[server.id]["TOGGLE"] = not self.settings["TOGGLE"]
+        if self.settings[server.id]["TOGGLE"]:
+            await self.bot.say("I will reply on mention.")
+        else:
+            await self.bot.say("I won't reply on mention anymore.")
+        dataIO.save_json("data/chatterbot/settings.json", self.settings)
+    
+    @chatterbot.command(pass_context=True)
+    @checks.mod_or_permissions(manage_channels=True)
+    async def channel(self, ctx, channel: discord.Channel):
+        """Toggles channel for automatic replies"""
+        server = ctx.message.server
+        if server.id not in self.settings:
+            self.settings[server.id] = {"TOGGLE" :False, "channel":""}
+        self.settings[server.id]["channel"] = channel.id
+        await self.bot.say("I will reply in {}".format(channel))
+        dataIO.save_json("data/chatterbot/settings.json", self.settings)
+
     async def on_message(self, message):
         server = message.server
         channel = message.channel
+        author = message.author
+        user = await self.bot.get_user_info(author.id)
+        conversation = []
+        if "http" in message.content or message.content == "" or author.bot:
+            return
         if server.id not in self.log:
             self.log[server.id] = {}
+            self.log[server.id][channel.id] = {"author":author.id,"message":message.content}
             dataIO.save_json("data/chatterbot/log.json", self.log)
         if channel.id not in self.log[server.id]:
-            self.log[server.id][channel.id] = message.clean_content
+            self.log[server.id][channel.id] = {"author":author.id,"message":message.content}
             dataIO.save_json("data/chatterbot/log.json", self.log)
+        last_author = self.log[server.id][channel.id]["author"]
+        last_message = self.log[server.id][channel.id]["message"]
+        if author.id == last_author and last_message != message.content:
+            self.log[server.id][channel.id]["message"] = message.content
+            dataIO.save_json("data/chatterbot/log.json", self.log)
+        if author.id != last_author and author.id != self.bot.user.id:
+            self.log[server.id][channel.id]["message"] = message.content
+            self.log[server.id][channel.id]["author"] = author.id
+            dataIO.save_json("data/chatterbot/log.json", self.log)
+            conversation.append(message.content)
+            conversation.append(last_message)
+            self.chatbot.train(conversation)
         if server.id not in self.settings:
             self.settings[server.id] = {"TOGGLE" :True, "channel":""}
             dataIO.save_json("data/chatterbot/settings.json", self.settings)
@@ -48,15 +91,16 @@ class Chatterbot():
             return
         if not self.bot.user_allowed(message):
             return
-        author = message.author
 
-        if message.author.id != self.bot.user.id:
+        if author.id != self.bot.user.id:
             to_strip = "@" + author.server.me.display_name + " "
             text = message.clean_content
             if not text.startswith(to_strip) and message.channel.id != self.settings[server.id]["channel"]:
                 return
             text = text.replace(to_strip, "", 1)
             await self.bot.send_typing(channel)
+            response = self.chatbot.get_response(text)
+            await self.bot.send_message(message.channel, response)
 
 
 
