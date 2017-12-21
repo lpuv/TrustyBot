10796798c4ba8655925fec03cbe789409fc86cfd
TrustyJAID <TrustyJAID@gmail.com>
2017-12-20 20:53:48 -0700

    Fixed key errors larger than 1024 bytes

--- cogs/chatterbot.py
+++ cogs/chatterbot.py
@@ -79,22 +79,22 @@ class Chatterbot():
             return
         if server.id not in self.log:
             self.log[server.id] = {}
-            self.log[server.id][channel.id] = {"author":author.id,"message":message.content}
+            self.log[server.id][channel.id] = {"author":author.id,"message":message.content[:1023]}
             dataIO.save_json("data/chatterbot/log.json", self.log)
         if channel.id not in self.log[server.id]:
-            self.log[server.id][channel.id] = {"author":author.id,"message":message.content}
+            self.log[server.id][channel.id] = {"author":author.id,"message":message.content[:1023]}
             dataIO.save_json("data/chatterbot/log.json", self.log)
         last_author = self.log[server.id][channel.id]["author"]
         last_message = self.log[server.id][channel.id]["message"]
-        if author.id == last_author and last_message != message.content:
-            self.log[server.id][channel.id]["message"] = message.content
+        if author.id == last_author and last_message != message.content[:1023]:
+            self.log[server.id][channel.id]["message"] = message.content[:1023]
             dataIO.save_json("data/chatterbot/log.json", self.log)
         if author.id != last_author and author.id != self.bot.user.id:
             try:
-                self.log[server.id][channel.id]["message"] = message.content
+                self.log[server.id][channel.id]["message"] = message.content[:1023]
                 self.log[server.id][channel.id]["author"] = author.id
                 dataIO.save_json("data/chatterbot/log.json", self.log)
-                conversation.append(message.content)
+                conversation.append(message.content[:1023])
                 conversation.append(last_message)
                 self.chatbot.train(conversation)
             except:
